9d196611199bddd7241af55d5672bfa6119a4516
DPLIT\zamad.t <zamad.t@dplit.com>
2023-05-09 01:24:39 +0500

    Responsive issues fixes

--- src/pages/referral-program.jsx
+++ src/pages/referral-program.jsx
@@ -2,21 +2,27 @@ const Referralprogram = () =>{
     return(
         <>
             <div className="banner">
-                <div className="banner__background landscape"></div>
-                <div className="banner__background rocks"></div>
-                <div className="banner__characters cartoon"></div>
+                <div className="banner__backgrounds">
+                    <div className="banner__backgrounds--image landscape"></div>
+                    <div className="banner__backgrounds--image rocks"></div>
+                    <div className="banner__backgrounds--image cartoon"></div>
+                </div>
+                
 
                 <div className="banner__content">
-                    <div className="container h-100">
-                    <div className="row h-100 align-items-center">
-                        <div className="col-md-7">
+                    <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-7">
                             <h1 className="banner__content--title">Get in Early on the<br/>$MBLK Pre-Sale</h1>
                             <p className="banner__content--desp">
                                 <strong>$MBLK</strong> is an ERC20 utility token on the <strong>Ethereum network</strong> that serves as the main in-game currency in <strong>Legends 
                                 of Bezogia</strong>. Developed by Zogi Labs, $MBLK offers players and investors a unique opportunity to be a part of the  
                                 <strong> future of gaming and blockchain technology</strong>.
                             </p>
-                            <div className="wallet">
+                     
+                        </div>
+                        <div className="col-md-12 col-lg-7">
+                        <div className="wallet">
                                 <div className="d-flex flex-nowrap justify-content-between mb-3 mb-md-3 ">
                                     <h2>Stage 1</h2>
                                     <h2>84.85% SOLD</h2>
--- src/shared-components/header.jsx
+++ src/shared-components/header.jsx
@@ -1,22 +1,45 @@
 const Header = () =>{
+    
+    const shoot = () => {
+        debugger
+        if(window.ethereum){
+            window.ethereum.request({method:'eth_requestAccounts'})
+            .then(res=>{
+                    alert('Your Wallet Address:: ' + res) 
+                    debugger
+                    window.ethereum.request({method:'eth_getBalance', params: [res[0], 'latest']})
+                    .then(balance => {
+                        debugger
+                        alert('Your Wallet Balance:: ' + balance) 
+                    })
+            })
+          }else{
+            alert("install metamask extension!!")
+          } 
+      }
     return(
         <div className="layout__header">
              <div className="container h-100">
                 <div className="layout__header--container">
-                <img src={process.env.PUBLIC_URL + "/assets/images/logo.svg"} alt="" />
-                <div className="layout__header--navmenu">
-                    <a href="">Intro Video</a>
-                    <a href="" className="active">Referral Program</a>
-                    <a href="">Buy now</a>
-                    <a href="">MBLK Vision</a>
-                    <a href="">Tokenomics</a>
-                    <a href="">how to buy</a>
-                    <a href="">how to buy</a>
-                </div>
-                <div className="layout__header--action-btns ">
-                    <button className="btn btn-outline-primary">Connect wallet</button>
-                    <button className="btn btn-primary">JOIN PRESALE</button>
-                </div>
+                    <img src={process.env.PUBLIC_URL + "/assets/images/logo.svg"} alt="" />
+                    <div className="layout__header--navmenu">
+                        <a href="">Intro Video</a>
+                        <a href="" className="active">Referral Program</a>
+                        <a href="">Buy now</a>
+                        <a href="">MBLK Vision</a>
+                        <a href="">Tokenomics</a>
+                        <a href="">how to buy</a>
+                        <a href="">how to buy</a>
+                    </div>
+                    <div className="layout__header--action-btns">
+                        <button className="btn btn-outline-primary" onClick={shoot}>Connect wallet</button>
+                        <button className="btn btn-primary">JOIN PRESALE</button>
+                    </div>
+                    <div className="layout__header--hamburger-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    </div>
                 </div>
              
              </div>
